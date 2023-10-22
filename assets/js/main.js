c45c2f8eeea7e8b89ab7db883445f90bc86e09ee
Lucas Machado <luccasmachado807@gmail.com>
2023-10-22 20:53:35 -0300

    Atualizando Layout

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+/**
+* Template Name: Siimple
+* Updated: Jul 27 2023 with Bootstrap v5.3.1
+* Template URL: https://bootstrapmade.com/free-bootstrap-landing-page/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  const toogleNav = function() {
+    let navButton = select('.nav-toggle')
+    navButton.classList.toggle('nav-toggle-active')
+    navButton.querySelector('i').classList.toggle('bx-x')
+    navButton.querySelector('i').classList.toggle('bx-menu')
+
+    select('.nav-menu').classList.toggle('nav-menu-active')
+  }
+  on('click', '.nav-toggle', function(e) {
+    toogleNav();
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.nav-menu .drop-down > a', function(e) {
+    e.preventDefault()
+    this.nextElementSibling.classList.toggle('drop-down-active')
+    this.parentElement.classList.toggle('active')
+  }, true)
+
+  /**
+   * Scrool links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      select('.nav-menu .active').classList.remove('active')
+      this.parentElement.classList.toggle('active')
+      toogleNav();
+    }
+  }, true)
+
+})()
+
+// Defina a data e hora alvo para o contador regressivo (substitua com sua própria data)
+const targetDate = new Date("2023-10-31T23:59:59").getTime();
+
+function updateCountdown() {
+    const currentDate = new Date().getTime();
+    const timeRemaining = targetDate - currentDate;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(days).padStart(2, "0");
+    document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, "0");
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, "0");
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerHTML = "<h2>Contagem regressiva encerrada!</h2>";
+    }
+}
+
+updateCountdown();
+const interval = setInterval(updateCountdown, 1000);
